5ffea4ef57f12b1a5bc555cfed86af7f55c96a3b
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-02-17 12:34:40 -0500

    Pull out Chron.jl internals (`using` and `@reexport` them instead)

---
+++ src/SubsidenceChron.jl
@@ -0,0 +1,23 @@
+module SubsidenceChron
+
+    using Reexport
+    @reexport using NaNStatistics
+    @reexport using StatGeochemBase
+    @reexport using Chron
+
+    # Basic statistics and UI resources
+    using ProgressMeter: @showprogress, Progress, update!
+
+    # Custom objects for holding SubsidenceChron age data
+    include("Objects.jl")
+
+    #Functions for subsidence modeling
+    include("SubsidenceStratMetropolis.jl")
+
+    # Structs
+    export StratData, ThermalSubsidenceParameters, SubsidenceStratAgeModel
+
+    # High-level functions
+    export DecompactBackstrip, SubsidenceStratMetropolis
+
+end # module
