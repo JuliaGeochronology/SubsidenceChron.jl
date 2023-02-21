1cd6286171b00aa1e2aa7c0ec0fc9d0c10f47707
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-02-20 21:19:17 -0500

    Test `decompact!` function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using SubsidenceChron
 using Test, Statistics, StatsBase
 
+@testset "Decompaction" begin
+    yₚ = zeros(3)
+    y = [1,2,3]
+    SubsidenceChron.decompact!(yₚ, y, 0.32, 1, 1, 1)
+    SubsidenceChron.decompact!(yₚ, y, 0.32, 1, 2, 1)
+    @test yₚ ≈ [0.0, 1.143612863313294, 2.1821135232335362]
+end
 @testset "Utilities" begin include("testUtilities.jl") end
 @testset "Eruption / deposition age distributions" begin include("testDist.jl") end
 @testset "Strat only" begin include("testStratOnly.jl") end
