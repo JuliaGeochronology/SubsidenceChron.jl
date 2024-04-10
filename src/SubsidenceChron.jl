ecfb34d0b692f51a4933d6b84cb4f1e0cc6d806f
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-04-10 10:49:23 -0400

    Add `find_formation_depths` function

--- src/SubsidenceChron.jl
+++ src/SubsidenceChron.jl
@@ -26,4 +26,14 @@ module SubsidenceChron
     # High-level functions
     export DecompactBackstrip, SubsidenceStratMetropolis
 
+    # Additional utility functions
+    function find_formation_depths(formation, thickness)
+        depth = cumsum([0; thickness])
+        unique_formations = unique(formation)
+        unique_formation_tops = [depth[findfirst(x->x==n, formation)] for n in unique_formations]
+        unique_formation_bottoms = [depth[findlast(x->x==n, formation)+1] for n in unique_formations]
+        return unique_formations, unique_formation_tops, unique_formation_bottoms
+    end
+    export find_formation_depths
+
 end # module
