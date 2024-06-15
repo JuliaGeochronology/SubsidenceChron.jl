08fe5e1b6972ad3ead3a980804615e9b2a632a92
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-06-15 16:43:34 -0400

    Add docstrings

--- src/SubsidenceStratMetropolis.jl
+++ src/SubsidenceStratMetropolis.jl
@@ -48,6 +48,27 @@ function reversecopyat!(dest, t, src)
     return dest
 end
 
+
+"""
+```julia
+SubsidenceStratMetropolis(smpl::ChronAgeData, config::StratAgeModelConfiguration, therm::ThermalSubsidenceParameters, subsidence_strat_depths, Sμ, Sσ, beta_ip, t0_ip;
+    subsidencebotom=minimum(smpl.Height),
+    subsidencetop=maximum(smpl.Height),
+    lithosphere = Normal(125000, 100),
+)
+```
+Runs the main SubsidenceChron.jl age-depth model routine given
+a set of Gaussian age constraints specified in the `smpl` struct,
+an age-depth model configuration specified in the `config` struct,
+thermal subsidence parameters defined in the `therm` struct,
+decompation and backstripping outputs in the form of `subsidence_strat_depths`, `Sμ`, and `Sσ`,
+and prior estimates for stretching factor Beta (`beta_ip`) and time of thermal subsedence onset (`t0_ip`).
+
+### Examples:
+```julia
+(subsmdl, agedist, lldist, beta_t0dist, lldist_burnin) = SubsidenceStratMetropolis(smpl, config, therm, subsidence_strat_depths, Sμ, Sσ_corr, Beta_sigma/10, T0_sigma/10)
+```
+"""
 function SubsidenceStratMetropolis(smpl::ChronAgeData, config::StratAgeModelConfiguration, therm::ThermalSubsidenceParameters, subsidence_strat_depths, Sμ, Sσ, beta_ip, t0_ip;
         subsidencebottom=minimum(smpl.Height),
         subsidencetop=maximum(smpl.Height),
--- src/Utilities.jl
+++ src/Utilities.jl
@@ -1,5 +1,17 @@
 
 # TODO: docstring
+"""
+```julia
+find_formation_depths(formation, thickness)
+```
+Find the top and bottom depth of each unique formation given a list of stratigraphic units and thicknesses
+
+### Examples:
+```julia
+ds = importdataset(joinpath("..", "examples", "Svalbard_highres.csv"), importas=:Tuple)
+formations, tops bottoms = find_formation_depths(ds.Formation, ds.Thickness)
+```
+"""
 function find_formation_depths(formation, thickness)
     depth = cumsum([0; thickness])
     unique_formations = unique(formation)
@@ -8,7 +20,18 @@ function find_formation_depths(formation, thickness)
     return unique_formations, unique_formation_tops, unique_formation_bottoms
 end
 
-# TODO: docstring
+"""
+```julia
+find_formation_heights(formation, thickness)
+```
+Find the top and bottom height of each unique formation given a list of stratigraphic units and thicknesses
+
+### Examples:
+```julia
+ds = importdataset(joinpath("..", "examples", "Svalbard_highres.csv"), importas=:Tuple)
+formations, tops bottoms = find_formation_heights(ds.Formation, ds.Thickness)
+```
+"""
 function find_formation_heights(formation, thickness)
     height = reverse!(cumsum(reverse!([thickness; 0])))
     unique_formations = unique(formation)
